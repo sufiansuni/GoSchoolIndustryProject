3d1dab181c9078e7645bf9ec98ad8fd6bd42e385
Sufian Suni <sufiansuni@gmail.com>
2021-08-24 10:22:39 +0800

    Update env.go
    - prints error when loading .env

--- env.go
+++ env.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -12,9 +13,10 @@ import (
 func goDotEnvVariable(key string) string {
 
 	// load .env file
-	err := godotenv.Load(".env")
+	err := godotenv.Load()
 
 	if err != nil {
+		fmt.Println(err)
 		log.Fatalf("Error loading .env file")
 	}
 
