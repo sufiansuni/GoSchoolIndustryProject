1e963206f2ecf5f96a1f3d863557996d877ab436
Sufian Suni <sufiansuni@gmail.com>
2021-08-31 21:42:23 +0800

    CRUD for users and sessions
    - Initial implementation of CRUD
    -- users
    -- sessions

--- database_session.go
+++ database_session.go
@@ -1,11 +1,39 @@
 package main
 
-// Operations for Session database: Insert, Delete, Update
+// Operations for sessions database: Insert(Create), Select(Read), Update, Delete
 
 // Insert a new session entry into database
 func insertSession(mySession session) error {
 	_, err := db.Exec("INSERT INTO sessions(UUID, Username) VALUES(?, ?)",
 		mySession.UUID, mySession.Username)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Select/Read a session entry from database with a UUID input
+func selectSession(UUID string) (session, error) {
+	var mySession session
+	query := "SELECT * FROM sessions WHERE UUID=?"
+
+	err := db.QueryRow(query, UUID).Scan(
+		&mySession.UUID,
+		&mySession.Username,
+	)
+	return mySession, err
+}
+
+// Update a session entry in database
+
+func updateSession(mySession session) error {
+	statement := "UPDATE sessions SET Username=? " +
+		"WHERE UUID=?"
+
+	_, err := db.Exec(statement,
+		mySession.Username,
+		mySession.UUID,
+	)
 	if err != nil {
 		return err
 	}
--- database_user.go
+++ database_user.go
@@ -1,6 +1,6 @@
 package main
 
-// Operations for User database: Insert, Delete, Update
+// Operations for users database: Insert(Create), Select(Read), Update, Delete
 
 // Insert a new user entry into database
 func insertUser(myUser user) error {
@@ -27,6 +27,70 @@ func insertUser(myUser user) error {
 		myUser.PostalCode,
 		myUser.Lat,
 		myUser.Lng)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Select/Read a user entry from database with a username input
+func selectUser(username string) (user, error) {
+	var myUser user
+	query := "SELECT * FROM users WHERE Username=?"
+
+	err := db.QueryRow(query, username).Scan(
+		&myUser.Username,
+		&myUser.Password,
+		&myUser.First,
+		&myUser.Last,
+		&myUser.Gender,
+		&myUser.Birthday,
+		&myUser.Height,
+		&myUser.Weight,
+		&myUser.CaloriesPerDay,
+		&myUser.Halal,
+		&myUser.Vegan,
+		&myUser.Address,
+		&myUser.PostalCode,
+		&myUser.Lat,
+		&myUser.Lng,
+	)
+	return myUser, err
+}
+
+// Update a user entry in database
+// Does not include username and password
+func updateUserProfile(myUser user) error {
+	statement := "UPDATE users SET First=?, Last=?, Gender=?, Birthday=?, " +
+		"Height=?, Weight=?, CaloriesPerday=?, Halal=?, Vegan=?, Address=?, PostalCode=?,  Lat=?, Lng=? " +
+		"WHERE Username=?"
+
+	_, err := db.Exec(statement,
+		myUser.First,
+		myUser.Last,
+		myUser.Gender,
+		myUser.Birthday,
+		myUser.Height,
+		myUser.Weight,
+		myUser.CaloriesPerDay,
+		myUser.Halal,
+		myUser.Vegan,
+		myUser.Address,
+		myUser.PostalCode,
+		myUser.Lat,
+		myUser.Lng,
+		myUser.Username,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete a user entry in database
+func deleteUser(username string) error {
+	_, err := db.Exec("DELETE FROM users where Username=?",
+		username)
 	if err != nil {
 		return err
 	}
